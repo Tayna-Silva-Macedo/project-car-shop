a786e33fc8205e0104b6617dd2b07daf7a7b5aa9
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-11-17 17:44:26 -0300

    test: testa se é possível criar um carro com sucesso

---
+++ tests/unit/Services/CarService/create.test.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import sinon from 'sinon';
+
+import Car from '../../../../src/Domains/Car';
+import ICar from '../../../../src/Interfaces/ICar';
+import CarService from '../../../../src/Services/CarService';
+import { createInput } from './mocks/Car';
+
+describe('Deveria cadastrar um novo carro', function () {
+  it('Cria um novo carro com SUCESSO', async function () {
+    const input: ICar = createInput;
+
+    const output: Car = new Car(createInput);
+
+    sinon.stub(Model, 'create').resolves(output);
+
+    const service = new CarService();
+    const result = await service.create(input);
+
+    expect(result).to.be.deep.equal(output);
+  });
+});
---
+++ tests/unit/Services/CarService/mocks/Car.ts
@@ -0,0 +1,23 @@
+const createInput = {
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.99,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+const createOutput = {
+  _id: '6376922a90cf9e9467abd2b6',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.99,
+  doorsQty: 4,
+  seatsQty: 5,
+  __v: 0,
+};
+
+export { createInput, createOutput };
