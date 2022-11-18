301bb2202d66259c9af4ee25e5ab0ac5ae131afb
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-11-18 16:22:10 -0300

    test: verifica que não é possível deletar um carro que não existe

---
+++ tests/unit/Services/CarService/delete.test.ts
@@ -0,0 +1,53 @@
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import sinon from 'sinon';
+
+import Car from '../../../../src/Domains/Car';
+import ICar from '../../../../src/Interfaces/ICar';
+import CarService from '../../../../src/Services/CarService';
+import { updateInput, updateOutput } from './mocks/Car';
+
+describe('Deveria deletar um carro pelo seu id', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it('Não é possível deletar um carro que não existe', async function () {
+    const idInput = '6377d86a83e61d159c277381';
+
+    sinon.stub(Model, 'findByIdAndDelete').resolves(null);
+
+    try {
+      const service = new CarService();
+      await service.destroy(idInput);
+    } catch (error) {
+      expect((error as Error).message).to.be.equal('Car not found');
+    }
+  });
+
+  // it('Não é possível atualizar um carro quando o formato do id esta inválido', async function () {
+  //   const idInput = 'abc123';
+  //   const objInput: ICar = updateInput;
+
+  //   try {
+  //     const service = new CarService();
+  //     await service.update(idInput, objInput);
+  //   } catch (error) {
+  //     expect((error as Error).message).to.be.equal('Invalid mongo id');
+  //   }
+  // });
+
+  // it('Atualiza um carro com SUCESSO', async function () {
+  //   const idInput = '634852326b35b59438fbea31';
+  //   const objInput: ICar = updateInput;
+
+  //   const output: Car = new Car(updateOutput);
+
+  //   sinon.stub(Model, 'findByIdAndUpdate').resolves(updateOutput);
+
+  //   const service = new CarService();
+  //   const result = await service.update(idInput, objInput);
+
+  //   expect(result).to.be.deep.equal(output);
+  // });
+});
